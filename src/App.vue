c05160039691a1f6f65e8d288cda12a5d08fbea8
rennavittorio <renna.vittorio@gmail.com>
2023-04-07 12:21:35 +0200

    start: file setup completed

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld,
+  }
+}
+
+</script>
+
+<template>
+
+  <HelloWorld 
+  msg="example"
+  />
+  
+</template>
+
+<style scoped>
+
+
+
+</style>
